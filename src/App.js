0f266835d28d3529b6de8201e60fb8fc3e5a4394
CbnMarian <m.marian03@yahoo.com>
2023-10-26 10:53:37 +0300

    add BG Nav Logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Navigation from "./Componenets/Navigation/Navigation";
+import Logo from "./Componenets/Logo/Logo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-
+      <Logo />
       {/* {<Logo/>
     <ImageLinkForm/>
     <FaceRecognition/>} */}
---
+++ src/Componenets/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import brain from "./Brain.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0  pointer">
+      <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner pa3 ">
+          <img style={{ paddingTop: 5 }} alt="logo" src={brain} />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
