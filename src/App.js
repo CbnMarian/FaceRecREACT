5f164e1d0e3d22e229ebba0331bd33cf261b2ea5
CbnMarian <m.marian03@yahoo.com>
2023-10-26 14:01:22 +0300

    section 2 before st funct

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Navigation from "./Componenets/Navigation/Navigation";
 import Logo from "./Componenets/Logo/Logo";
+import ImageLinkForm from "./Componenets/ImageLinkForm/ImageLinkForm";
+import Rank from "./Componenets/Rank/Rank";
+import ParticlesBg from "particles-bg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#63014e" num={200} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
       {/* {<Logo/>
     <ImageLinkForm/>
     <FaceRecognition/>} */}
--- src/Componenets/Logo/Logo.js
+++ src/Componenets/Logo/Logo.js
@@ -5,9 +5,9 @@ import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0  pointer">
-      <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
-        <div className="Tilt-inner pa3 ">
+    <div className="ma4 mt0  pointer ">
+      <Tilt className="Tilt br2 shadow-2 " style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner pa3">
           <img style={{ paddingTop: 5 }} alt="logo" src={brain} />
         </div>
       </Tilt>
