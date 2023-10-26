5e4d7580f13f04ec3f0a16088670945266ec25e5
CbnMarian <m.marian03@yahoo.com>
2023-10-26 17:01:18 +0300

    added functionality for searchbar and button, but reciving error from reading urls outputs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,93 @@
 import React, { Component } from "react";
 import Navigation from "./Componenets/Navigation/Navigation";
+import FaceRecognition from "./Componenets/FaceRecognition/FaceRecognition";
 import Logo from "./Componenets/Logo/Logo";
 import ImageLinkForm from "./Componenets/ImageLinkForm/ImageLinkForm";
 import Rank from "./Componenets/Rank/Rank";
 import ParticlesBg from "particles-bg";
 import "./App.css";
 
+//start api clarifai  the api key   f9058498712748b8bc93d9a1be8af8cd
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = "6aa83b23bb1e4c5e8460fdd33c2f662f";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "cbnmarian";
+  const APP_ID = "cbnmarian";
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = "face-detection";
+
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+  return requestOptions;
+};
+///////////////////////////////////////////////////////////////
+
+//  DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
+
+//end clarifai
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
     };
   }
 
-  onInputChange = (event) => {
-    console.log(event.target.value);
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+      returnClarifaiRequestOptions(this.state.input)
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("hi", response);
+      })
+      .then(function (response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+
+        //ok until here
+      });
   };
 
-  onButtonSubmit = () => {
-    console.log("click");
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
   };
 
   render() {
     return (
       <div className="App">
-        <ParticlesBg color="#63014e" num={200} type="cobweb" bg={true} />
+        <ParticlesBg color="#63014e" num={300} type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
@@ -33,9 +95,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* {<Logo/>
-    <ImageLinkForm/>
-  <FaceRecognition/>} */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
