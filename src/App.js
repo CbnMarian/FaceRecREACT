41ad7f9f1e62cdd85f4c508b85b3b694aa5933e6
CbnMarian <m.marian03@yahoo.com>
2023-10-27 02:16:27 +0300

    added sign in portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import Navigation from "./Componenets/Navigation/Navigation";
+import ParticlesBg from "particles-bg";
 import FaceRecognition from "./Componenets/FaceRecognition/FaceRecognition";
+import Navigation from "./Componenets/Navigation/Navigation";
+import Signin from "./Componenets/Signin/signin";
+import Register from "./Componenets/Register/Register";
 import Logo from "./Componenets/Logo/Logo";
 import ImageLinkForm from "./Componenets/ImageLinkForm/ImageLinkForm";
 import Rank from "./Componenets/Rank/Rank";
-import ParticlesBg from "particles-bg";
 import "./App.css";
 
 const returnClarifaiRequestOptions = (imageUrl) => {
@@ -15,7 +17,6 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   const USER_ID = "cbnmarian";
   const APP_ID = "cbnmarian";
   // Change these to whatever model and image URL you want to use
-  const MODEL_ID = "face-detection";
 
   const IMAGE_URL = imageUrl;
 
@@ -54,6 +55,8 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false,
     };
   }
 
@@ -65,14 +68,13 @@ class App extends Component {
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.topRow * height,
+      topRow: clarifaiFace.top_row * height,
       rightCol: width - clarifaiFace.right_col * width,
       bottomRow: height - clarifaiFace.bottom_row * height,
     };
   };
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({ box: box });
   };
 
@@ -96,18 +98,41 @@ class App extends Component {
     //ok until here
   };
 
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
     return (
       <div className="App">
-        <ParticlesBg color="#63014e" num={300} type="cobweb" bg={true} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <ParticlesBg color="#63014e" num={140} type="cobweb" bg={true} />
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
